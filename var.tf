b7d597dc8522bce46e94e45f48fc21fe5686e5cd
Ankit sahu <66328783+an618@users.noreply.github.com>
2023-11-03 00:04:49 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,4 +1,9 @@
 # add this  var.tf file in main file where port is given
+#WE can also add cidr clock
+variable "publiccidr"{
+type = list(string)
+default = ["0.0.0.0/0"]
+}
 variable "server_port"{
 type = number
 default = 80
