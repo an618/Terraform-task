b83b8e23067159d70ca086959d75898afb6082ac
Ankit sahu <66328783+an618@users.noreply.github.com>
2023-11-02 14:19:09 +0530

    Create main.tf
    creating instance using terraform

---
+++ main.tf
@@ -0,0 +1,57 @@
+#  providing access_key and secret_key in code
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAZYPIH46FXCBBQXSS"
+  secret_key = "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"
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-05c0f5389589545b7"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=[aws_security_group.ownsg.id]  # --> this will attach sg to this instance
+  key_name="tf-key-pair"  # this will attach key_pair to instance
+
+# giving name to instance
+
+tags={
+ Name="terraform-plan"
+}
+}
+
+# adding inbound rule
+
+resource "aws_security_group" "ownsg" {
+ name="ownsg"
+ingress {
+ from_port=80
+ to_port=80
+protocol="tcp"
+cidr_blocks= ["0.0.0.0/0"]
+}
+ingress {
+ from_port=22
+ to_port=22
+protocol="tcp"
+cidr_blocks= ["0.0.0.0/0"]
+}
+# Adding outbound rule
+egress {
+ from_port=0
+ to_port=0
+protocol="-1"
+cidr_blocks= ["0.0.0.0/0"]
+}
+}
+
+# creating and adding key pair
+
+resource "aws_key_pair" "tf-key-pair" {
+key_name = "tf-key-pair"
+public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits  = 4096            #bydefault port
+}
+resource "local_file" "tf-key" {
+content  = tls_private_key.rsa.private_key_pem
+filename = "tf-key-pair"
