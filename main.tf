3ee41b5c9fd0405b1a5680a3b227353ac6c7a2b5
Ankit sahu <66328783+an618@users.noreply.github.com>
2023-11-03 13:31:25 +0530

    Add files via upload
    terramodule

--- main.tf
+++ main.tf
@@ -1,57 +1,37 @@
-#  providing access_key and secret_key in code
 provider "aws" {
+  access_key=" "
+  secret_key= " "
   region     = "ap-south-1"
-  access_key = "access_key"
-  secret_key = "secret_key "
 }
-resource "aws_instance" "myec2" {
-  ami           = "ami-05c0f5389589545b7"
-  instance_type = "t2.micro"
-  vpc_security_group_ids=[aws_security_group.ownsg.id]  # --> this will attach sg to this instance
-  key_name="tf-key-pair"  # this will attach key_pair to instance
-
-# giving name to instance
-
-tags={
- Name="terraform-plan"
-}
-}
-
-# adding inbound rule
-
-resource "aws_security_group" "ownsg" {
- name="ownsg"
-ingress {
- from_port=80
- to_port=80
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}
-ingress {
- from_port=22
- to_port=22
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}
-# Adding outbound rule
-egress {
- from_port=0
- to_port=0
-protocol="-1"
-cidr_blocks= ["0.0.0.0/0"]
-}
-}
-
-# creating and adding key pair
-
 resource "aws_key_pair" "tf-key-pair" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
 algorithm = "RSA"
-rsa_bits  = 4096            #bydefault port
+rsa_bits  = 4096
 }
 resource "local_file" "tf-key" {
 content  = tls_private_key.rsa.private_key_pem
 filename = "tf-key-pair"
+}
+module "vpc"{
+source= "./vpcmodule/"
+}
+module "web" {
+source= "./webmodule/"
+subnet_id=module.vpc.websubnet_id
+vpc_id=module.vpc.vpcid
+}
+module "app"{
+source= "./appmodule/"
+subnet_id=module.vpc.appsubnet_id
+vpc_id=module.vpc.vpcid
+}
+module "db"{
+source= "./dbmodule/"
+vpc_id= module.vpc.vpcid
+subnet_id= module.vpc.dbsubnet_id
+appsubnetid= module.vpc.appsubnet_id
+}
+
